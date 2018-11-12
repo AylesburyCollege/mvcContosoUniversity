15eef4e5b19c9561d5905dd348a6bd839fc9031a
DerekPeacock <derek.peacock@yahoo.com>
2018-11-12 16:36:33 +0000

    Added a view model for some enrollment statistics in the about view

--- mvcContosoUniversity/Controllers/HomeController.cs
+++ mvcContosoUniversity/Controllers/HomeController.cs
@@ -1,13 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Linq;
 using System.Web.Mvc;
+using mvcContosoUniversity.Models;
+using mvcContosoUniversity.ViewModels;
 
 namespace mvcContosoUniversity.Controllers
 {
     public class HomeController : Controller
     {
+        private ContosoContext db = new ContosoContext();
+
         public ActionResult Index()
         {
             return View();
@@ -15,9 +16,14 @@ namespace mvcContosoUniversity.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
+            IQueryable<EnrollmentDateGroup> data = from student in db.Students
+                                                   group student by student.EnrollmentDate into dateGroup
+                                                   select new EnrollmentDateGroup()
+                                                   {
+                                                       EnrollmentDate = dateGroup.Key,
+                                                       StudentCount = dateGroup.Count()
+                                                   };
+            return View(data.ToList());
         }
 
         public ActionResult Contact()
@@ -25,6 +31,12 @@ namespace mvcContosoUniversity.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
         }
     }
 }
